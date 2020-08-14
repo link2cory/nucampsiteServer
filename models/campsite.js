af6cb443ed2c6de7689294ef0753f7a96290b79d
Cory Perkins <link2cory@gmail.com>
2020-08-14 13:18:39 -0700

    add integration with mongodb database through mongoose for campsites endpoints

---
+++ models/campsite.js
@@ -0,0 +1,64 @@
+var mongoose = require("mongoose");
+require("mongoose-currency").loadType(mongoose);
+
+const Schema = mongoose.Schema;
+const Currency = mongoose.Types.Currency;
+
+const commentSchema = new Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      requires: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const campsiteSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    comments: [commentSchema],
+    image: {
+      type: String,
+      required: true,
+    },
+    elevation: {
+      type: Number,
+    },
+    cost: {
+      type: Currency,
+      required: true,
+      min: 0,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Campsite = mongoose.model("Campsite", campsiteSchema);
+
+module.exports = Campsite;
